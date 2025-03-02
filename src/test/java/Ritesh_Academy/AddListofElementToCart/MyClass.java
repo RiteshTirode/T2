a76705c8bfdd41ebf06c9c46d6ed890f4f3b6eac
RiteshTirode <riteshtirode1992@gmail.com>
2025-03-02 19:27:07 +0530

    message by T1

--- src/test/java/Ritesh_Academy/AddListofElementToCart/MyClass.java
+++ src/test/java/Ritesh_Academy/AddListofElementToCart/MyClass.java
@@ -5,6 +5,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My Class");
+		System.out.println("Comment by T1");
 	}
 
 }
