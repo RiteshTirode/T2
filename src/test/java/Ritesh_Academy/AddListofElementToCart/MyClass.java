3178445fb6be468c6e238fd6e5a0779397110e92
RiteshTirode <riteshtirode1992@gmail.com>
2025-03-02 19:20:30 +0530

    1st commit by t2

---
+++ src/test/java/Ritesh_Academy/AddListofElementToCart/MyClass.java
@@ -0,0 +1,10 @@
+package Ritesh_Academy.AddListofElementToCart;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Class");
+	}
+
+}
